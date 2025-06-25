7e520dc03e04377a773922984ea31b542a1dd84b
amir0050 <amirollabergenov5@gmail.com>
2025-06-25 18:52:41 +0500

    Create home3.py

---
+++ lesson-3/homework/home3.py
@@ -0,0 +1,91 @@
+#fruit=['apple','orange','banana','mango','kiwi']
+#print(fruit[3])
+
+#firslist=[1,2,3,'a']
+#secondlist=[4,5,'b']
+#con=firslist+secondlist
+#print(con)
+
+
+
+"""numbers = [10, 20, 30, 40, 50, 60, 70]
+first = numbers[0]
+middle = numbers[len(numbers) // 2]
+last = numbers[-1]
+result = [first, middle, last]
+print("Extracted elements:", result)"""
+
+
+'''movie=['vector','simba','transformer','war','folks']
+favtuple=tuple(movie)
+print(favtuple)'''
+
+
+'''cities =[
+    "New York",
+    "London",
+    "Tokyo",
+    "Paris",
+    "Sydney",
+    "Berlin",
+    "Toronto",
+    "Dubai",
+    "Rome",
+    "Singapore"]
+jon=" ".join(cities)
+findparis=jon.find("Paris")
+print(findparis)'''
+
+
+#num=[1,2,3,4,5]
+#duplicate=num*2
+#print(duplicate)
+
+'''num=[1,2,3,4,5]
+num[0],num[-1]=num[-1],num[0]
+print(num)'''
+
+
+'''num=(1,2,3,4,5,6,7,8,9,10)
+slicee=num[3:7]
+print(slicee)'''
+
+#color=['blue','pink','red']
+#countcolor=color.count('blue')
+#print(countcolor)
+
+
+'''animals=('lion','tiger','dolphin','shark','white lion')
+findindex=animals.index('lion')
+print(findindex)'''
+
+'''
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+merged_tuple = tuple1 + tuple2
+print("Merged Tuple:", merged_tuple)'''
+
+
+
+'''my_list = [10, 20, 30, 40]
+my_tuple = (5, 15, 25)
+print("Length of List:", len(my_list))
+print("Length of Tuple:", len(my_tuple))'''
+
+
+
+'''tuple_numbers = (100, 200, 300, 400, 500)
+list_numbers = list(tuple_numbers)
+print("Converted List:", list_numbers)'''
+
+
+
+'''numbers = (8, 3, 15, 1, 22, 7)
+print("Maximum:", max(numbers))
+print("Minimum:", min(numbers))'''
+
+
+
+'''words = ("apple", "banana", "cherry", "date", "elderberry")
+reversed_words = words[::-1]
+print("Reversed Tuple:", reversed_words)'''
