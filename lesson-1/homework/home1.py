42e0fcaf95c07d4603131dcadce5ff345d28c8ab
amir0050 <amirollabergenov5@gmail.com>
2025-06-25 20:21:33 +0500

    Create home1.py

---
+++ lesson-1/homework/home1.py
@@ -0,0 +1,26 @@
+import math
+
+side = 5
+perimeter = 4 * side
+area = side ** 2
+print("Square - Perimeter:", perimeter, "Area:", area)
+
+
+
+diameter = 10
+circle_length = math.pi * diameter
+print("Circle - Length:", circle_length)
+
+
+a = 7
+b = 13
+mean = (a + b) / 2
+print("Mean of", a, "and", b, "is:", mean)
+
+
+
+sum_ab = a + b
+product_ab = a * b
+square_a = a ** 2
+square_b = b ** 2
+print("Sum:", sum_ab, "Product:", product_ab, "Square of a:", square_a, "Square of b:", square_b)
